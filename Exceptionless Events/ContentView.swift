5c05514fb19d2b35dacd8e19aa368a3fca1c02d7
polluterofminds <justin.edward.hunter@gmail.com>
2021-04-02 08:24:43 -0500

    Initial Commit

---
+++ Exceptionless Events/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Exceptionless Events
+//
+//  Created by Justin Hunter on 4/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Exceptionless Events/Exceptionless_EventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exceptionless_EventsApp.swift
+//  Exceptionless Events
+//
+//  Created by Justin Hunter on 4/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Exceptionless_EventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
